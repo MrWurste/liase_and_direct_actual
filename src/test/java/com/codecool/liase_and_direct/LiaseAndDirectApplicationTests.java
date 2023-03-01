b831a5167b83bae8bdd4aab7a10d8617b3939f86
MrWurste <wadid1199@gmail.com>
2023-03-01 10:01:31 +0100

    Initial commit

---
+++ src/test/java/com/codecool/liase_and_direct/LiaseAndDirectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.liase_and_direct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiaseAndDirectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
