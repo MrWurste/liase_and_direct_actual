c14dc165fa108d075c4c3e6598da9d8f92b7b163
MrWurste <wadid1199@gmail.com>
2023-03-07 12:39:42 +0100

    Changes by lukry - helped to resolve database tables relations

---
+++ src/main/java/com/codecool/liase_and_direct/repos/AnnouncementRepository.java
@@ -0,0 +1,16 @@
+package com.codecool.liase_and_direct.repos;
+
+import com.codecool.liase_and_direct.models.announcements.Announcement;
+import com.codecool.liase_and_direct.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Repository of Announcement. Because it extends JpaRepository no implementation is needed.
+ * Model: typeOfMethiod 'keyWord''fieldFromClass'(parameter type)
+ */
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
+    List<Announcement> findAllByShowToIn(Set<User> s);
+}
